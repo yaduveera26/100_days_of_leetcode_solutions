f87af25200d2e0871321410309863e87a047e0c4
yaduveera26 <49535410+yaduveera26@users.noreply.github.com>
2020-10-21 13:08:25 +0530

    852

---
+++ 1.5_852.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l=0,r=arr.size()-1;
+        while(l<r)
+        {
+            int m = l+(r-l)/2;
+            if(arr[m]>arr[m-1]&&arr[m]>arr[m+1])
+                return m;
+            else if(arr[m]>arr[m-1]&&arr[m]<arr[m+1])
+                l=m;
+            else
+                r=m;
+        }
+        return l;
+    }
+};
