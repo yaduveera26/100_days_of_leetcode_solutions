eaecd8dba006d6100e89d1199dd1222ddfe79156
yaduveera26 <49535410+yaduveera26@users.noreply.github.com>
2020-10-21 14:03:41 +0530

    704

---
+++ 1.9_704.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size();
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            if(nums[m]==target)
+                return m;
+            else if(nums[m]>target)
+                r=m-1;
+            else
+                l=m+1;
+        }
+        return -1;
+    }
+};
