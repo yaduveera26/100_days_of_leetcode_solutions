8552eb4440349a6382a8e7c9b17ffe0e4388b2b7
yaduveera26 <49535410+yaduveera26@users.noreply.github.com>
2020-10-21 14:08:48 +0530

    350

---
+++ 1.10_350.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums2.begin(),nums2.end());
+        sort(nums1.begin(),nums1.end());
+        vector<int>v;
+        int l=0,r=0;
+        while(l<nums1.size()&&r<nums2.size())
+        {
+            if(nums1[l]==nums2[r])
+            {
+                v.push_back(nums1[l]);
+                l+=1;
+                r+=1;
+            }
+            else if(nums1[l]>nums2[r])
+                r+=1;
+            else
+                l+=1;
+        }
+        return v;
+    }
+};
