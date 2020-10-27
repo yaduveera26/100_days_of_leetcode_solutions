56dc94bb68d9569257f95b815ca9840c9865d1f3
yaduveera26 <49535410+yaduveera26@users.noreply.github.com>
2020-10-27 10:03:56 +0530

    Create 1.42_64.cpp

---
+++ 1.42_64.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[i].size();j++)
+            {
+                if(i==0&&j==0)
+                    continue;
+                else if(i==0&&j!=0)
+                    grid[i][j]=grid[i][j-1]+grid[i][j];
+                else if(i!=0&&j==0)
+                    grid[i][j]=grid[i-1][j]+grid[i][j];
+                else
+                grid[i][j]=min(grid[i][j-1],grid[i-1][j])+grid[i][j];
+            }
+        }
+        return grid[grid.size()-1][grid[0].size()-1];
+    }
+};
