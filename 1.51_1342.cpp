53b40a0d5bddbea59c74a580502075bd6d5f8b0b
yaduveera26 <49535410+yaduveera26@users.noreply.github.com>
2020-11-01 18:58:42 +0530

    Create 1.51_1342.cpp

---
+++ 1.51_1342.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSteps (int num) {
+        int cnt=0;
+        while(num>0)
+        {
+            if(num%2)
+                num-=1;
+            else
+                num/=2;
+            cnt+=1;
+        }
+        return cnt;
+    }
+};
