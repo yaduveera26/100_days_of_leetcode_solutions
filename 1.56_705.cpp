afe85b267d1bb59c304a9419a92eab12f99b047c
yaduveera26 <49535410+yaduveera26@users.noreply.github.com>
2020-11-01 19:34:01 +0530

    Create 1.56_705.cpp

---
+++ 1.56_705.cpp
@@ -0,0 +1,28 @@
+class MyHashSet {
+public:
+    long int a[1000000]={0};
+    /** Initialize your data structure here. */
+    MyHashSet() {
+    }
+    
+    void add(int key) {
+        a[key]=1;
+    }
+    
+    void remove(int key) {
+        a[key]=0;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        return a[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
